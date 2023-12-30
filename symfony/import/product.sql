ec0b85f5826db3e312f9c0e3d017fa9653e887be
ckaotik <151904+ckaotik@users.noreply.github.com>
2023-12-30 23:53:50 +0100

    Change demo products language

--- symfony/import/product.sql
+++ symfony/import/product.sql
@@ -3,8 +3,8 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 INSERT INTO `product` (`id`, `name`, `description`, `image_url`, `price`, `created`, `status`) VALUES
-(1, 'Boot', 'Ein altehrwürdiges Boot im rustikalen Ambiente.', 'https://picsum.photos/id/211/300/200.jpg', 9999999, '2023-12-25 11:14:44', 1),
-(2, 'Oldtimer Auto', 'Diese wunderschönen Oldtimer-Modelle gibt es jetzt in mehreren Farben zum Sonderpreis. Nur solange der Vorrat reicht!', 'https://picsum.photos/id/133/300/200.jpg', 4999900, '2023-12-25 11:18:44', 1),
-(3, 'Kinder-Laufrad', 'Bringt den Nachwuchs auf Touren! Das einmalige Laufrad der Marke Eigenbau ist ausschließlich in unserem Shop erhältlich.', 'https://picsum.photos/id/146/300/200.jpg', 29900, '2023-12-25 11:20:28', 1),
-(4, 'Seilbahn', 'Ihre höchst eigene Seilbahn. Das kann wahrlich nicht jeder von sich sagen! Beeindrucken Sie Ihre Nachbarn und zeigen Sie allen, was Sie sich leisten können. Kein Berg zur Hand? Nutzen Sie unser XXS Modell für den Weg ins Obergeschoss. Nie wieder Treppen steigen!', 'https://picsum.photos/id/328/300/200.jpg', 999999999, '2023-12-25 11:23:46', 0);
+(1, 'Boat', 'A time-honored boat in a rustic ambience.', 'https://picsum.photos/id/211/300/200.jpg', 9999999, '2023-12-25 11:14:44', 1),
+(2, 'Oldtimer car', 'These beautiful vintage models are now available in several colors at a special price. Only while stocks last!', 'https://picsum.photos/id/133/300/200.jpg', 4999900, '2023-12-25 11:18:44', 1),
+(3, 'Balance bike', 'Get the little ones going! The unique balance bike of the brand Eigenbau is only available in our store.', 'https://picsum.photos/id/146/300/200.jpg', 29900, '2023-12-25 11:20:28', 1),
+(4, 'Cable car', 'Your very own cable car. Not everyone can say that about themselves! Impress your neighbors and show everyone what you can afford. No mountain at hand? Use our XXS model to get to the second floor. Never climb stairs again!', 'https://picsum.photos/id/328/300/200.jpg', 999999999, '2023-12-25 11:23:46', 0);
 COMMIT;
